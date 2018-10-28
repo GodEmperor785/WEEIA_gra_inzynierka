5ab408f144d4b0fc8095bf109e7d5d4ab6abb249
godemperor785 <godemperor785@gmail.com>
2018-10-28 11:54:34 +0100

    Make login run in loop until success or disconnect, add better logging

---
+++ GAME_Server/GAME_Server/FailureReasons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME_Server {
+	public static class FailureReasons {
+		public static readonly string INVALID_INTERNAL_PACKET = "Invalid type of internal packet";
+		public static readonly string INVALID_PACKET_TYPE = "Invalid packet OperationType received";
+		public static readonly string INCORRECT_LOGIN = "Incorrect login data for username: ";
+		public static readonly string USERNAME_ALREADY_EXISTS = "Username already exists: ";
+		public static readonly string CLIENT_DISCONNECTED = "Connection ended without proper disconnect";
+	}
+}
